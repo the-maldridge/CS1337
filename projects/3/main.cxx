10a32299605d9ced07f3b8fa5e410adb31ff7cb2
Michael Aldridge <aldridge.mac@gmail.com>
2014-02-25 13:45:04 -0600

    added makefile and added initial declarations to main.cxx

--- projects/3/main.cxx
+++ projects/3/main.cxx
@@ -4,3 +4,11 @@ It reads in SSV answer keys and student response files
 Then spits out SSV grade report files
 */
 
+#include <iostream>
+#include <fstream>
+#include <cctype>
+using namespace std;
+
+int main(int argc, char** argv) {
+  return 0;
+}

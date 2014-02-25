6a8c63dd52895b26a0da5d4cc4dbeb64c755bb51
Michael Aldridge <aldridge.mac@gmail.com>
2014-02-25 13:36:09 -0600

    fixed selection sort

--- inClass/02.20.14/selsort.cxx
+++ inClass/02.20.14/selsort.cxx
@@ -4,11 +4,12 @@ using namespace std;
 
 
 char* selsort(char* ptr, int len) {
-  int lowMemLoc=&ptr;
-  for(int i=0; i<len; i++) {
-    for(int j=i; j<len; j++) {
+  int lowMemLoc;
+  for(int i=0; i<len-1; i++) {
+    lowMemLoc=i;
+    for(int j=i+1; j<len; j++) {
       if(*(ptr+j)<*(ptr+lowMemLoc)) {
-	  lowMemLoc = ptr+j;
+	  lowMemLoc = j;
 	}
     }
     swap(*(ptr+i), *(ptr+lowMemLoc));
@@ -21,19 +22,22 @@ int main() {
   int size;
   cout << "how many letters to alphabet" << endl;
   cin >> size;
-  char foo = new char[size];
-
+  char* foo = new char[size];
+  
   for(int i=0; i<size; i++) {
     cout << "Enter a letter: ";
     cin >> *(foo+i);
   }
 
   for(int i=0; i<size; i++) {
-    cout << *(foo+i) << " " << endl;
-  }
+    cout << *(foo+i) << " ";
+    }
+  cout << endl;
   foo = selsort(foo, size);
   for(int i=0; i<size; i++) {
-    cout << *(foo+i) << " " << endl;
+    cout << *(foo+i) << " ";
   }
+  cout << endl;
+
   return 0;
 }

6e35ba346a3da092b715f7d51c7277b5d682f3bb
Michael Aldridge <aldridge.mac@gmail.com>
2014-03-01 23:37:35 -0600

    fixed c_string issue with the init function

--- projects/3/main.cxx
+++ projects/3/main.cxx
@@ -19,19 +19,21 @@ Then spits out SSV grade report files
 //back to normal stuff
 using namespace std;
 
-/*
+
 bool init(string answers, string tests) {
-  ifstream key(answers);
-  ifstream toGrade(tests);
+  ifstream key(answers.c_str());
+  ifstream toGrade(tests.c_str());
   int tSize;
 
   key >> tSize; //find out how long the test was
+  FILE_LOG(logINFO) << "Your selected key contains " << tSize << " answers.";
 
 }
-*/
+
 
 int main(int argc, char** argv) {
-  FILELog::ReportingLevel() = FILELog::FromString(argv[1]?argv[1]:"DEBUG1");
-  FILE_LOG(logDEBUG1) << "Program Start";
+  FILELog::ReportingLevel() = FILELog::FromString(argv[1]?argv[1]:"DEBUG");
+  FILE_LOG(logDEBUG) << "Program Start";
+  init("answers.txt", "exams.txt");
   return 0;
 }

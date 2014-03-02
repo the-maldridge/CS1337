d8e47b038709dbf4adca9931b32e46c72a3e2deb
Michael Aldridge <aldridge.mac@gmail.com>
2014-03-02 00:04:59 -0600

    now loads the answer key

--- projects/3/main.cxx
+++ projects/3/main.cxx
@@ -23,16 +23,34 @@ using namespace std;
 bool init(string answers, string tests) {
   ifstream key(answers.c_str());
   ifstream toGrade(tests.c_str());
-  int tSize;
+  string bounce;
+  int tSize; //length of the test
+  int cSize; //size of the class taking it
 
   key >> tSize; //find out how long the test was
   FILE_LOG(logINFO) << "Your selected key contains " << tSize << " answers.";
 
+  //build a place to put the answers
+  char* ansKey = new char[tSize];
+
+  //read in the answers to the key
+  for(int i=0; i<tSize; i++) {
+    key >> *(ansKey+i);
+    FILE_LOG(logDEBUG1) << "Read answer: " << i + 1 << ":" << *(ansKey+i);
+  }
+
+  //do some absurd file stuff to account for inconsistencies in the 
+  //provided file spec -- please revise rubric for next semester
+  for(int i=0; getline(toGrade, bounce); i++) {
+    cSize++;
+    FILE_LOG(logDEBUG1) << "Read exam: " << i +1 << ":" << bounce;
+  }
+  FILE_LOG(logINFO) << "The test file appears to have " << cSize << " tests";
 }
 
 
 int main(int argc, char** argv) {
-  FILELog::ReportingLevel() = FILELog::FromString(argv[1]?argv[1]:"DEBUG");
+  FILELog::ReportingLevel() = FILELog::FromString(argv[1]?argv[1]:"DEBUG1");
   FILE_LOG(logDEBUG) << "Program Start";
   init("answers.txt", "exams.txt");
   return 0;

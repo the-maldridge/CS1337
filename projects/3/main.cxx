d24c96a9b6d0573125be22920cd38f8e1c1626ef
Michael Aldridge <aldridge.mac@gmail.com>
2014-03-01 23:32:03 -0600

    added logging stuff

--- projects/3/main.cxx
+++ projects/3/main.cxx
@@ -6,9 +6,32 @@ Then spits out SSV grade report files
 
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <cctype>
+
+//pull in logging stuff
+#ifdef LOG1
+#include "log1.h"
+#else
+#include "log.h"
+#endif
+
+//back to normal stuff
 using namespace std;
 
+/*
+bool init(string answers, string tests) {
+  ifstream key(answers);
+  ifstream toGrade(tests);
+  int tSize;
+
+  key >> tSize; //find out how long the test was
+
+}
+*/
+
 int main(int argc, char** argv) {
+  FILELog::ReportingLevel() = FILELog::FromString(argv[1]?argv[1]:"DEBUG1");
+  FILE_LOG(logDEBUG1) << "Program Start";
   return 0;
 }

d510ba05b8aa7504cc71b45bb0e9e019efb8871f
Michael Aldridge <aldridge.mac@gmail.com>
2014-03-23 19:27:20 -0500

    added main file and makefile

---
+++ projects/4/main.cxx
@@ -0,0 +1,15 @@
+/*
+Revenge of the database, need I say more?
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+  return 0;
+}
